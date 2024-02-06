8783e9be1701287b6d55077d1457a843ce492403
AGQA2024 <codesoftware2579@gmail.com>
2024-02-06 12:28:30 +0000

    First commit to add files.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
+
+        {/* Making basic navigation buttons: */}
+        <div>
+          <input type="button" value="Click Me!"/>
+        </div>
+
+      {/* Setting up navigation links: */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<h1>Landing Page</h1>}/>
+            <Route path="home">
+              <Route index element = {<h1>Home Page</h1>}/>
+              <Route path="about" element = {<h1>About Page</h1>}/>
+              <Route path="team" element = {<h1>Team Page</h1>}/>
+              <Route path="support" element = {<h1>Home Page</h1>}/>
+              <Route path="contact" element = {<h1>Home Page</h1>}/>
+            </Route>
+          </Route>
+          {/* <Route path="/home" element = {<h1>Home Page</h1>}/> */}
+          {/* <Route path="/about" element = {<h1>Home Page</h1>}/> */}
+          {/* <Route path="/team" element = {<h1>Home Page</h1>}/> */}
+          {/* <Route path="/support" element = {<h1>Home Page</h1>}/> */}
+          {/* <Route path="/contact" element = {<h1>Home Page</h1>}/> */}
+        </Routes>
+      </BrowserRouter>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
